3ac1cbb7a6384930131ee68f30ed3cefbb760bf3
nguyenlenam-18112000 <61400842+nguyenlenam-18112000@users.noreply.github.com>
2020-06-03 19:41:09 +0700

    Update Sentences.java

--- OOP_Project/src/process/Sentences.java
+++ OOP_Project/src/process/Sentences.java
@@ -2,21 +2,26 @@ package process;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
-
-public class Sentences {
-	private List<String> listModel = new ArrayList<>();
-	public String getIndex(int index) {
-		return listModel.get(index);
+/**
+ * Chứa các câu kết quả
+ * @author Ngốc_Học_OOP
+ *
+ */
+public abstract class Sentences {
+	/**
+	 * list dùng chung, các câu sau khi xử lý sẽ được cho hết vào list này
+	 */
+	protected static List<String> listSentences = new ArrayList<>();
+	/**
+	 * <h1> Hàm thêm các câu trong list các câu output vào listSentences</h1>
+	 * <p> Đầu vào: List các câu output
+	 * @param list
+	 */
+	public void add(List<String> list) {
+		for (String s:list) {
+			listSentences.add(s);
+		}
 	}
-	public void setIndex(int index,String st) {
-		listModel.set(index, st);
-	}
-	public void addSentences(String st) {
-		listModel.add(st);
-	}
-	public int size() {
-		return listModel.size();
-	}
+	
+	
 }
